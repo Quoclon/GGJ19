4a45e121fa2d0d96a2eff4b911a18bd1e0462e22
Quoclon <Brodendangio@gmail.com>
2019-01-25 22:25:26 -0800

    First Commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float moveSpeed = -0.50f;
+    swipe swipeDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swipeDirection = GetComponent<swipe>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(swipeDirection.direction == 0)
+        {
+            transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+
+        }
+        else
+        {
+            transform.Translate(swipeDirection.direction * Time.deltaTime, 0, 0);
+        }
+
+
+    }
+}
---
+++ Assets/swipe.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swipe : MonoBehaviour
+{
+
+    Vector2 startPos, endPos;
+    public float direction;
+    float touchStartTime, touchFinishTime, timeInterval;
+    bool canSwipe = false;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //checkInputMouse();
+        //checkInputTouch();
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (canSwipe)
+            {
+                touchStartTime = Time.time;
+                startPos = Input.mousePosition;
+                Debug.Log("touch start");
+            }
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (canSwipe)
+            {
+                touchFinishTime = Time.time;
+                endPos = Input.mousePosition;
+                Debug.Log("touch end");
+
+                timeInterval = touchFinishTime - touchStartTime;
+                direction = -(startPos.x - endPos.x);
+                if(direction > 0)
+                {
+                    direction = 0.50f;
+                }
+                else
+                {
+                    direction = -0.50f;
+                }
+
+
+                Debug.Log("Direction: " + direction);
+
+            }
+        }
+
+
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Entered");
+        canSwipe = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        canSwipe = false;
+        Debug.Log("exited");
+    }
+}
