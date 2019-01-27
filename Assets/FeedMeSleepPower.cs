4a2b8343f916516cb85fc94387c355c12972a987
dvdmrn <dgm.marino@gmail.com>
2019-01-27 12:05:22 -0800

    UI without links

---
+++ Assets/FeedMeSleepPower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedMeSleepPower : MonoBehaviour
+{
+    Slider slider;
+    public GameObject sleepPower;
+    
+    void Start(){
+        slider = GetComponent<Slider>();
+    }
+
+    void Update(){
+        if(sleepPower==null){
+            print("WARNING: you did not connect an object containging sleep power");
+        }
+
+        // ===========================================\\
+        // !!! uncomment below and ajust as needed
+
+        // slider.value = sleepPower.VARIABLE_CONTAINING_SLEEP_POWER
+        // ===========================================//
+            
+
+    }
+}
+
---
+++ Assets/UpdateTextWithValue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateTextWithValue : MonoBehaviour
+{
+    TextMeshProUGUI textmeshPro;
+    int value;
+    public GameObject scoreContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textmeshPro = GetComponent<TextMeshProUGUI>();
+        print("tmp: "+textmeshPro);
+        //==============================================\\
+        // !!! uncomment and replace link w/ appropriate var
+
+        // value = scoreContainer.VARIABLE_NAME_HERE;
+        //==============================================//
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textmeshPro.SetText("{0}",value);
+    }
+}
