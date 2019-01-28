5c72df2166d1ed4c0764ea4620eb72cfe8908abb
dvdmrn <dgm.marino@gmail.com>
2019-01-27 17:41:02 -0800

    better fizzin

--- Assets/Fizzle.cs
+++ Assets/Fizzle.cs
@@ -7,40 +7,27 @@ public class Fizzle : MonoBehaviour
 {
     Movement mov;
     public float delay; //seconds to wait before fizzle
+    Vector3 ls;
+
+    bool animating = false;
+    float iterator = 1.5f;
+    float decayRate = 0.02f;
+
+
     // Start is called before the first frame update
     void Start()
     {
         mov = GetComponent<Movement>();
-        print(Easing.Elastic.Out(0f));
+        ls=transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "SwipeZone")
-        {
-            print("entered!!!");
-            StartCoroutine("FizzleOut");
-        }
-    }
-
-    IEnumerator FizzleOut(){
-        Vector3 ls=transform.localScale;
-        yield return new WaitForSeconds(delay);
-        float frameRate = 0.0008f;
-        bool animating = true;
-        float iterator = 1f;
-        float decayRate = 0.001f;
-        while(animating){
+        if(animating){
             float expand = Easing.Elastic.Out(iterator);
-            iterator -= 0.01f;
+            iterator -= decayRate;
             transform.localScale = new Vector3(ls.x*expand,ls.y*expand,0f);
-            yield return new WaitForSeconds(frameRate);
             // print("iter: "+expand);
             if(iterator<=0f){
                 animating=false;
@@ -48,4 +35,23 @@ public class Fizzle : MonoBehaviour
             }
         }
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "SwipeZone")
+        {
+            animating=true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision){
+        if(collision.tag == "SwipeZone")
+        {
+            animating=false;
+            print("EXITED");
+        }
+
+    }
+
+
 }
